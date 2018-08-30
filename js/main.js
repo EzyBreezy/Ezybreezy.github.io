74f845992e7f0c0134a43b599341628ded582640
Ezana Bahta <ezanab2@gmail.com>
2018-08-30 11:56:29 -0400

    added different pages to link about, cv, contact, pagedown

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var timeStamp = function {
+    document.write("This page was last modified on: " + document.lastModified +"");
+}; 
+//sourced time stamp format http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.W4gJuZNKiL9
+//it doesnt work currently but im sure after todays lesson this can be revisited to produce an output.
